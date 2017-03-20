2e0a706e5537a10adddd30fc735dd7cfb0289265
PYTEC <PYTEC@users.noreply.github.com>
2017-03-20 20:11:50 +0100

    Update episode.py

--- sickrage/tagger/episode.py
+++ sickrage/tagger/episode.py
@@ -29,6 +29,7 @@ class EpisodeTags(object):
             u'mpeg': tags.mpeg,
             u'xvid': tags.xvid,
             u'netflix': tags.netflix,
+            u'amazon': tags.amazon,
         }
 
     def _get_match_obj(self, attr, regex=None, flags=0):
@@ -122,6 +123,8 @@ class EpisodeTags(object):
             return 'dlmux'
         if self.netflix: 
             return self.netflix
+        if self.amazon:
+            return self.amazon
         else:
             attr = 'web'
             match = self._get_match_obj(attr)
@@ -265,9 +268,18 @@ class EpisodeTags(object):
     @property
     def netflix(self):
         """
-        Netflix tage found in name
+        Netflix tag found in name
         :return: an empty string if not found
         """
         attr = 'netflix'
         match = self._get_match_obj(attr)
         return '' if not match else match.group()
+    @property
+    def amazon (self):
+        """
+        Amazon tag found in name
+        :return: an empty string if not found
+        """
+        attr = 'amazon'
+        match = self._get_match_obj(attr)
+        return '' if not match else match.group
