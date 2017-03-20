1ae02b9aa282cf04b58fa70b55f5212a75a701f4
PYTEC <PYTEC@users.noreply.github.com>
2017-03-20 20:09:04 +0100

    Add AmazonHD/UHD, NetflixHD/UHD, VODHD und BD

--- sickrage/recompiled/tags.py
+++ sickrage/recompiled/tags.py
@@ -8,11 +8,12 @@ resolution = re.compile(r'(?P<vres>4320|2160|1080|720|480|360)(?P<scan>[pi])', r
 # Sources
 tv = re.compile(r'([sph]d).?tv|tv(rip|mux)', re.I)
 dvd = re.compile(r'(?P<hd>hd)?dvd(?P<rip>rip|mux)?', re.I)
-web = re.compile(r'(web(?P<type>rip|mux|hd|.?dl|\b))', re.I)
-bluray = re.compile(r'(blue?-?ray|b[rd](?:rip|mux))', re.I)
+web = re.compile(r'(web(?P<type>rip|mux|hd|.?dl|\b)|vodhd)', re.I)
+bluray = re.compile(r'(blue?-?ray|b[rd](?:rip|mux)|bd)', re.I)
 sat = re.compile(r'(dsr|satrip)', re.I)
-itunes = re.compile(r'(itunes)', re.I)
+itunes = re.compile(r'itunes(hd|uhd)', re.I)
 netflix = re.compile(r'netflix(hd|uhd)', re.I)
+amazon = re.compile(r'amazon(hd|uhd)', re.I)
 
 # Codecs
 avc = re.compile(r'([xh].?26[45])', re.I)
